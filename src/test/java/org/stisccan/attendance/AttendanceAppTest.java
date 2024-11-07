b04826cd30d85abc81f5d0944da40b26a51bb17c
akifyuksel <akif42@live.nl>
2024-11-08 00:01:03 +0100

    setup meuk.

---
+++ src/test/java/org/stisccan/attendance/AttendanceAppTest.java
@@ -0,0 +1,13 @@
+package org.stisccan.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
